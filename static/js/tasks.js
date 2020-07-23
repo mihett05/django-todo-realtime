638128e690d425a9727ad8f3bdde95c35e62136e
mihett05 <mihett05@yandex.ru>
2020-07-23 13:09:41 +0500

    Added handling changes on client

--- static/js/tasks.js
+++ static/js/tasks.js
@@ -2,7 +2,7 @@ let current_todo_list = [];
 
 const render_tasks = (todo_list) => {
     const tasks = document.querySelector("#tasks");
-    current_todo_list = todo_list;
+    current_todo_list = [...todo_list];
     tasks.innerHTML = "";
 
     todo_list.forEach((v) => {
@@ -11,7 +11,34 @@ const render_tasks = (todo_list) => {
         el.setAttribute("text", v.text);
         tasks.append(el);
     });
-};
+}
+
+const parse_change = (data) => {
+    let todo_list = [...current_todo_list];
+    if (data.type === "create") {
+        let find = todo_list.filter((v) => v.id === data.task.id);
+        if (find.length === 0) {
+            todo_list.push(data.task);
+        }
+    } else if (data.type === "edit") {
+        todo_list.forEach((v, i) => {
+            if (v.id === data.task.id) {
+                todo_list[i] = data.task;
+            }
+        });
+    } else if (data.type === "delete") {
+        let index = null;
+        todo_list.forEach((v, i) => {
+            if (v.id === data.task.id) {
+                index = i;
+            }
+        });
+        if (index != null) {
+            todo_list.splice(index, 1);
+        }
+    }
+    render_tasks(todo_list);
+}
 
 const create_task_action = (text) => {
     create_task(text)
--- static/js/ws.js
+++ static/js/ws.js
@@ -1,15 +1,15 @@
 const ws = new WebSocket(`ws://${location.host}/ws/todo`);
 
-ws.onopen = () => {
-    console.log("open");
-}
-
 ws.onclose = (event) => {
     console.log(event);
 }
 
 ws.onmessage = (event) => {
-    console.log(event);
+    try {
+        let data = JSON.parse(event.data);
+        parse_change(data);
+    } catch (e) {}
+
 }
 
 ws.onerror = (error) => {
